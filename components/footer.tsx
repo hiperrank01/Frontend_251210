b0e4c47a29dc8b78e3c0e5eeba37ba65705fe9c3
hiperrank01 <hiperrank01@users.noreply.github.com>
2026-01-01 12:11:23 +0900

    update: footer 주소 변경

--- components/footer.tsx
+++ components/footer.tsx
@@ -16,11 +16,12 @@ export const Footer = () => {
               />
             </div>
 
-            <p>데이터 분석 기반 매출 최적화 전문</p>
-            <p>대표가 웹사이트를 만들 줄 압니다</p>
+            <p>데이터 분석 기반 매출 최적화</p>
+            {/* <p>대표가 웹사이트를 만들 줄 압니다</p> */}
+            
           </div>
           <div className="space-y-2 text-sm text-gray-400">
-            <p>주소:강서구 글로벌마케팅센터 a동 201호</p>
+            <p>주소:서울 강서구 공항대로61길 29 글로벌마케팅센터 a동 201호</p>
             <p>전화: 010-4590-4917</p>
             <p>이메일:9winit01@gmail.com</p>
             <p>사업자번호: 246-17-02470</p>
@@ -28,7 +29,7 @@ export const Footer = () => {
           </div>
         </div>
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 나인위닛. All rights reserved.</p>
+          <p>&copy; 2025 나인위닛. All rights reserved.</p>
         </div>
       </div>
     </footer>
