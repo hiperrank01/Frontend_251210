0d5fa0b139eb5dde6fc7e8baf38ddf47998f60f6
daegeunbae <antoniobae12@gmail.com>
2026-04-11 11:57:36 +0900

    update: seo update01

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://ninewinit.store"),
-  title: "나인위닛 - 네이버 구글 메타 자동화 마케팅 솔루션",
+  title: "나인위닛 - 네이버 구글 메타 광고 최적화 전문",
   description:
     "네이버 구글 메타 광고 최적화 해드립니다. 매출 성장을 위한 마케팅 솔루션을 제공합니다.",
   keywords: "스마트스토어, SEO, 네이버쇼핑, 광고최적화, 키워드분석",
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     url: "https://ninewinit.store",
-    title: "나인위닛 - 스마트스토어 SEO 최적화 솔루션",
+    title: "나인위닛 - 네이버 구글 메타 광고 최적화 전문",
     description:
       "네이버 구글 메타 광고 최적화 해드립니다. 매출 성장을 위한 마케팅 솔루션을 제공합니다.",
     siteName: "Ninewinit",
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
 
   twitter: {
     card: "summary_large_image",
-    title: "나인위닛 - 스마트스토어 SEO 최적화 솔루션",
+    title: "나인위닛 - 네이버 구글 메타 광고 최적화 전문",
     description: "네이버 구글 메타 광고 최적화 해드립니다. 매출 성장을 위한 마케팅 솔루션을 제공합니다",
     images: ["/Logo_Main.png"],
   },
